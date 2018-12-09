e8962e2942f9b7a42b0689cb05f6ae5aa89cf7c0
Mark Hernandez <mark@lion-byte.com>
2018-12-08 18:10:09 -0600

    Initialize Gatsby

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MissingPage = props => (
+  <div>
+    <h1>Woops!</h1>
+  </div>
+)
+
+export default MissingPage
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HomePage = props => (
+  <div>
+    <h1>Hello!</h1>
+  </div>
+)
+
+export default HomePage
