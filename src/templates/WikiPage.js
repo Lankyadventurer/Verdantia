5350e720d4475cabb73abecbb25281425620a6ad
Mark Hernandez <mark@lion-byte.com>
2018-12-29 23:47:53 -0600

    Add edit link per wiki page

--- src/templates/WikiPage.js
+++ src/templates/WikiPage.js
@@ -2,12 +2,19 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import styled from 'styled-components'
+import { FaPen } from 'react-icons/fa'
 
 import Layout from '../components/Layout'
-import { normalizeURL } from '../shared/url'
+import { filenameToURL, normalizeURL } from '../shared/url'
 
 const WikiEntry = styled.article`
+  .edit-link {
+    float: right;
+  }
+
   .description {
+    clear: both;
+
     code {
       background-color: ${props => props.theme.offWhite};
       padding: 0 0.75ch;
@@ -19,6 +26,7 @@ const WikiEntry = styled.article`
  * @param {object} props
  * @param {object} props.data
  * @param {object} props.data.wiki
+ * @param {string} props.data.wiki.fileAbsolutePath
  * @param {object} props.data.wiki.frontmatter
  * @param {string} props.data.wiki.frontmatter.category
  * @param {string} props.data.wiki.frontmatter.title
@@ -35,6 +43,18 @@ export const WikiPage = props => {
 
       <WikiEntry>
         <h1>{wiki.frontmatter.title}</h1>
+
+        <a
+          className='edit-link'
+          href={`/admin/#/collections/wiki/entries/${filenameToURL(
+            wiki.fileAbsolutePath,
+            '.md'
+          )}`}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Edit <FaPen />
+        </a>
 
         <p>
           Category:{' '}
@@ -57,6 +77,7 @@ export default WikiPage
 export const query = graphql`
   query WIKI_PAGE_QUERY($id: String!) {
     wiki: markdownRemark(id: { eq: $id }) {
+      fileAbsolutePath
       frontmatter {
         category
         title
